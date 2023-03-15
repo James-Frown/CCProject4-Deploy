f56b1b2071edb8fddf472bf4b8bbf41be8834fd2
James-Frown <21122@ctca.co.za>
2023-03-15 11:54:16 +0200

    Updating the pricings cards

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -1,25 +1,30 @@
 import '../App.css'
 import React from 'react'
+import PricingsCard from './PricingsCard'
 
 function Pricing() {
 
-    return (
+  return (
 
-        <div id="Pricing" className="Pricing">
-        <div className="Pricing__Wrapper">
-          <h1 className="Heading">
-            Start Today!
-          </h1>
-          <p className="Description">
-            Affordable and transparent digital dashboard pricing.
-          </p>
-          <div className="Pricing__Cards">
-            
-          </div>
+    <div id="Pricing" className="Pricing">
+      <div className="Pricing__Wrapper">
+        <h1 className="Heading">
+          Start Today!
+        </h1>
+        <p className="Description">
+          Affordable and transparent digital dashboard pricing.
+        </p>
+        <div className="Pricing__Cards">
+
+          <PricingsCard Name="Educational Plan" Bio="For Students ad Lectures!" Price="$10.00" Date="Billed Yearly" />
+          <PricingsCard Name="Basic Plan" Bio="For Start Up Business's!" Price="$60.00" Date="Billed Monthly" />
+          <PricingsCard Name="Enteprize Professional Plan" Bio="The Full package for coporate use!" Price="$250.00" Date="Billed Monthly" />
+
         </div>
       </div>
+    </div>
 
-    )
+  )
 }
 
 export default Pricing
--- src/components/pricingsCard.jsx
+++ src/components/pricingsCard.jsx
@@ -1,12 +1,40 @@
 import '../App.css'
 
-function pricingsCard(props) {
+import { BsFillPatchCheckFill } from 'react-icons/bs'
+
+function PricingsCard(props) {
+
+  const Name = props.Name;
+  const Bio = props.Bio;
+  const Price = props.Price;
+  const Date = props.Date;
+
+  const Labels = props.Labels;
   
-    return (
-      <div className="Flixcard">
-        
+
+  return (
+    <div className="Pricings__Card">
+      <div className="Pricings__Card__Top">
+        <p className="Pricings__Plan__Name">
+          {Name}
+        </p>
+        <p className="Pricings__Plan__Bio">
+          {Bio}
+        </p>
       </div>
-    )
-  }
-  
-  export default pricingsCard
+      <div className="Pricings__Card__Middle">
+        <p className="Pricings__Plan__Price">
+          {Price}
+        </p>
+        <p className="Pricings__Plan__Date">
+          {Date}
+        </p>
+      </div>
+      <div className="Pricings__Card__Bottom">
+        <BsFillPatchCheckFill size={20} />
+      </div>
+    </div>
+  )
+}
+
+export default PricingsCard
