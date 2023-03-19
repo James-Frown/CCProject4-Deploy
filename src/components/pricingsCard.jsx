d05ee14e96aeca2a0cf24667086a4e4397486fdd
James-Frown <21122@ctca.co.za>
2023-03-19 12:09:52 +0200

    Updates
    Changed the Pricings Cards, Changed the responsiveness of pricing cards and pricing wrapper, changed the reponsiveness of the testemonoials wrapper

--- src/components/pricingsCard.jsx
+++ src/components/pricingsCard.jsx
@@ -1,16 +1,11 @@
 import '../App.css'
 
-import { BsFillPatchCheckFill } from 'react-icons/bs'
-
 function PricingsCard(props) {
 
   const Name = props.Name;
   const Bio = props.Bio;
   const Price = props.Price;
   const Date = props.Date;
-
-  const Labels = props.Labels;
-
 
   return (
     <div className="Pricings__Card">
@@ -29,9 +24,6 @@ function PricingsCard(props) {
         <p className="Pricings__Plan__Date">
           {Date}
         </p>
-      </div>
-      <div className="Pricings__Card__Bottom">
-        <BsFillPatchCheckFill size={20} />
       </div>
     </div>
   )
