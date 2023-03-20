111f41a86a221d17774d60a88c1a13feab5cd5b5
James-Frown <21122@ctca.co.za>
2023-03-20 17:09:02 +0200

    new ways of doing navigation

--- src/components/NewNavigation.jsx
+++ src/components/NewNavigation.jsx
@@ -6,16 +6,13 @@ import { GiHamburgerMenu } from 'react-icons/Gi'
 import { GrClose } from 'react-icons/Gr'
 
 
-function NewNavigation() {
-
+function NewNavigation () {
     return (
-
         <header>
             <nav className="Nav__Bar">
                 <a href="#Hero" className="Nav__Brand">
                     WorkFlex
                 </a>
-
                 <ul className="Nav__Menu">
                     <li className="Nav__Item">
                         <a href="#Features" className="Nav__Link">Features</a>
@@ -33,18 +30,12 @@ function NewNavigation() {
                         <a href="#Contact" className="Nav__Link">Contact Us</a>
                     </li>
                 </ul>
-
                 <button type="" className="DropDown">
                     <div className="Hamburger__Open__Icon"><GiHamburgerMenu size={30} /></div>
                     <div className="Hamburger__Close__Icon"><GrClose size={30} /></div>
                 </button>
-
-
             </nav>
         </header>
-
-
-
     )
 }
 
